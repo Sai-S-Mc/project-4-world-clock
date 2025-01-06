86e5b556db76af7746b583a927b17a6230d3dd43
Sai-S-Mc <sai.satti97@gmail.com>
2025-01-06 13:07:59 -0500

    Added a select element for the user to choose between 12 & 24 hr formats (not fully functional)
    Currently debugging this feature. The initial clock format select works but the subsequent selects get overwritten by the clock format that is selected the second time

--- src/script.js
+++ src/script.js
@@ -34,15 +34,49 @@ function updateTime() {
   }
 }
 
-function handleSelection(event) {
+function twentyFourHourClock(event) {
   let timeZone = event.target.value;
-  if (timeZone === "current"){
+  console.log(`----Running 24 hour clock function----`);
+  console.log(`The timeZone is ${timeZone}`);
+  console.log(`The length of the timeZone is ${timeZone.length}`);
+  if (timeZone === "current") {
+    console.log(`current timezone condition is a pass`);
     timeZone = moment.tz.guess();
   }
-    if (timeZone.length > 1) {
-      let cityName = timeZone.replace("_", " ").split("/")[1];
-      let cityElement = document.querySelector("#cities");
-      cityElement.innerHTML = `<div class="each-city">
+  if (timeZone.length > 1) {
+    console.log(
+      `condition that the timezone length should be > 1 for a 24 hr clock is a pass`
+    );
+
+    let cityName = timeZone.replace("_", " ").split("/")[1];
+    let cityElement = document.querySelector("#cities");
+    cityElement.innerHTML = `<div class="each-city">
+    <div class="city-and-date">
+    <h2>${cityName}</h2>
+    <div class="date">${moment().tz(timeZone).format("dddd, MMM D, YYYY")}</div>
+      </div>
+      <div class="time">${moment().tz(timeZone).format("H:mm:ss")} 
+          </div></div><a href="/" class="homepage">Back to homepage</a>`;
+    console.log(`City, date and time in 24 hr format have now been added`);
+  }
+}
+
+function twelveHourClock(event) {
+  let timeZone = event.target.value;
+  console.log(`----Running 12 hour clock function----`);
+  console.log(`The timeZone is ${timeZone}`);
+  console.log(`The length of the timeZone is ${timeZone.length}`);
+  if (timeZone === "current") {
+    console.log(`current timezone condition is a pass`);
+    timeZone = moment.tz.guess();
+  }
+  if (timeZone.length > 1) {
+    console.log(
+      `condition that the timezone length should be > 1 for a 12 hr clock is a pass`
+    );
+    let cityName = timeZone.replace("_", " ").split("/")[1];
+    let cityElement = document.querySelector("#cities");
+    cityElement.innerHTML = `<div class="each-city">
     <div class="city-and-date">
     <h2>${cityName}</h2>
     <div class="date">${moment().tz(timeZone).format("dddd, MMM D, YYYY")}</div>
@@ -50,14 +84,34 @@ function handleSelection(event) {
       <div class="time">${moment()
         .tz(timeZone)
         .format("h:mm:ss")} <small>${moment()
-        .tz(timeZone)
-        .format("A")}</small></div>
+      .tz(timeZone)
+      .format("A")}</small></div>
           </div><a href="/" class="homepage">Back to homepage</a>`;
-    }
+    console.log(`City, date and time in 12 hr format have now been added`);
+  }
 }
 
-let selectCityElement = document.querySelector("#select-city");
-selectCityElement.addEventListener("change", handleSelection);
+function displaySelectedCity(event) {
+  let selectedTimeFormat = event.target.value;
+  console.log(`~~~The selected time format is ${selectedTimeFormat}~~~`);
+  console.log(
+    `The length of the selected time format is ${selectedTimeFormat.length}`
+  );
+  if (selectedTimeFormat.length > 1) {
+    if (selectedTimeFormat === "twelve-hour") {
+      console.log(`12 hour condition is a pass`);
+      let selectCityElement = document.querySelector("#select-city");
+      selectCityElement.addEventListener("change", twelveHourClock);
+    } else {
+      console.log(`24 hour condition is a pass`);
+      let selectCityElement = document.querySelector("#select-city");
+      selectCityElement.addEventListener("change", twentyFourHourClock);
+    }
+  }
+}
+
+let selectTimeFormatElement = document.querySelector("#select-time-format");
+selectTimeFormatElement.addEventListener("change", displaySelectedCity);
 
 updateTime();
 setInterval(updateTime, 10);
